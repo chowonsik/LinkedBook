83ac86e560966d7ed37646549bfc61f9841a3aa1
multicampus <plain3785@gmail.com>
2021-07-27 14:21:25 +0900

    feat: Add user profile info (S05P12B307-105)

--- front/src/components/Follow/FollowerItem/index.jsx
+++ front/src/components/Follow/FollowerItem/index.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import './style.scss';
-function FollowerItem({profileImage, followerID}) {
-    return (
-        <div className="follower-item">
-            <div className="profile-image">
-                <img src={profileImage} alt={followerID} />
-            </div>
-            <div className="follower-id">
-                <h3>{followerID}</h3>
-            </div>
-            <div className="cancel-btn">
-                <button>삭제</button>
-            </div>
-        </div>
-    )
+import React from "react";
+import "./style.scss";
+function FollowerItem({ profileImage, followerID }) {
+  return (
+    <div className="follower-item">
+      <div className="profile-image">
+        <img src={profileImage} alt={followerID} />
+      </div>
+      <div className="follower-id">
+        <h3>{followerID}</h3>
+      </div>
+      <div className="cancel-btn">
+        <button>삭제</button>
+      </div>
+    </div>
+  );
 }
 
-export default FollowerItem
+export default FollowerItem;
--- front/src/routes/FollowerList/index.jsx
+++ front/src/routes/FollowerList/index.jsx
@@ -1,26 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import FollowerItem from '../../components/Follow/FollowerItem';
+import React, { useState, useEffect } from "react";
+import FollowerItem from "../../components/Follow/FollowerItem";
 
-import './style.scss';
+import "./style.scss";
 function FollowerList() {
-    // redux를 사용했을 때는 user 내의 follower 들을 불러오도록 함
-    const [follower, setFollower] = useState({});
-    useEffect(() => {
-        handleSetFollower()
-    }, [])
+  // redux를 사용했을 때는 user 내의 follower 들을 불러오도록 함
+  const [follower, setFollower] = useState({});
+  useEffect(() => {
+    handleSetFollower();
+  }, []);
 
-    function handleSetFollower() {
-        const user = {
-            profileImage: "https://drive.google.com/uc?id=1tf7W-Kb9p7eAfRbPx1fVCRrpPvyJSueR",
-            followerID: "정다은",
-        }
-        setFollower(user);
-    }
-    return (
-        <div className="follower-list">
-            <FollowerItem profileImage={follower.profileImage} followerID={follower.followerID}/>
-        </div>
-    )
+  function handleSetFollower() {
+    const user = {
+      profileImage:
+        "https://drive.google.com/uc?id=1tf7W-Kb9p7eAfRbPx1fVCRrpPvyJSueR",
+      followerID: "정다은",
+    };
+    setFollower(user);
+  }
+  return (
+    <div className="follower-list">
+      <FollowerItem
+        profileImage={follower.profileImage}
+        followerID={follower.followerID}
+      />
+    </div>
+  );
 }
 
-export default FollowerList
+export default FollowerList;
