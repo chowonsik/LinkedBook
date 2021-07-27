990d460e849279e35367b8fb07e0d59f27b9b4a3
dwbyun16 <dwbyun16@gmail.com>
2021-07-27 14:28:12 +0900

    feat: Add user signin/signup page (S05P12B307-50, S05P12B307-86)

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,13 @@
-import { HashRouter as Router, Route} from 'react-router-dom';
-import './App.css';
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import "./App.css";
+import SignIn from "./routes/user/SignIn";
+import SignUp from "./routes/user/SignUp";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-      
-      </Router>
+      <SignIn />
+      {/* <SignUp /> */}
     </div>
   );
 }
