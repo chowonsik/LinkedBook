ab91074bd77d12b47edc6779174cf169cc443afe
kimmeh <mhko411@smail.kongju.ac.kr>
2021-07-27 13:07:15 +0900

    feat: Add Following List (S05P12B307-53)

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,16 @@
+import React from 'react';
 import { HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import FollowerList from './routes/FollowerList';
+import FollowingList from './routes/FollowingList';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route to="/profile/follower/list" exact={true} component={FollowerList} />
+        
+        <Route path='/profile/follower/list' exact={true} component={FollowerList} />
+        <Route path='/profile/following/list' exact={true} component={FollowingList} />
       </Router>
     </div>
   );
