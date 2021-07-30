4d29465271a8d2af8de9a295845e5d74b862604d
multicampus <plain3785@gmail.com>
2021-07-30 11:24:39 +0900

    feat: Add user tab (S05P12B307-106)

--- front/src/components/Profile/UserInfo/index.jsx
+++ front/src/components/Profile/UserInfo/index.jsx
@@ -4,7 +4,7 @@ function UserInfo({ userObj }) {
   return (
     <section className="profile">
       <div className="profile-content">
-        <img className="profile-img" src={userObj.image} alt="profile image" />
+        <img className="profile-img" src={userObj.image} alt="profile" />
         <div className="profile-info">
           <div className="profile-title">
             <h1 className="username">{userObj.nickname}</h1>
@@ -12,9 +12,9 @@ function UserInfo({ userObj }) {
             <i className="fas fa-cog" aria-label="update profile"></i>
           </div>
           <div className="profile-detail">
-            <strong className="location">{userObj.dongmyeonri}</strong>
+            <strong className="location">{userObj.dong}</strong>
             <i className="fas fa-smile-wink manner" alt="매너온도">
-              <span>{userObj.manner}</span>
+              <span>{userObj.mannerScore}</span>
             </i>
           </div>
         </div>
