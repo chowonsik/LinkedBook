42657e288b5a3b33413884af6486430c5a7f1691
wscho94 <wscho94@gmail.com>
2021-08-11 23:22:06 +0900

    feat: Add spring security apply with jwt (S05P13B307-194)

--- backend/src/main/java/com/linkedbook/response/ResponseStatus.java
+++ backend/src/main/java/com/linkedbook/response/ResponseStatus.java
@@ -13,18 +13,12 @@ public enum ResponseStatus {
      * 2XX Success
      */
     // 200 OK - 클라이언트의 요청을 서버가 정상적으로 처리했다.
-    SUCCESS_SELECT_USERAREA(200, 204, "유저 거래지역 조회에 성공하였습니다."),
-    SUCCESS(200, 200, "요청에 성공하였습니다."), 
-    SUCCESS_SIGN_IN(200, 201, "로그인에 성공하였습니다."),
-    SUCCESS_SELECT_AREA(200, 204, "지역 조회에 성공하였습니다."),
-    SUCCESS_GET_FOLLOW_LIST(200, 202, "팔로우 리스트를 가져오는데 성공하였습니다."),
-    SUCCESS_SELECT_DEAL_LIST(200, 205, "거래 조회에 성공하였습니다."), 
-    SUCCESS_UPDATE_DEAL(200, 206, "거래 수정에 성공하였습니다."),
-    SUCCESS_DELETE_DEAL(200, 207, "거래 삭제에 성공하였습니다."), 
+    SUCCESS_SELECT_USERAREA(200, 204, "유저 거래지역 조회에 성공하였습니다."), SUCCESS(200, 200, "요청에 성공하였습니다."),
+    SUCCESS_SIGN_IN(200, 201, "로그인에 성공하였습니다."), SUCCESS_SELECT_AREA(200, 204, "지역 조회에 성공하였습니다."),
+    SUCCESS_GET_FOLLOW_LIST(200, 202, "팔로우 리스트를 가져오는데 성공하였습니다."), SUCCESS_SELECT_DEAL_LIST(200, 205, "거래 조회에 성공하였습니다."),
     SUCCESS_SELECT_DEAL_DETAIL(200, 208, "거래 상세 조회에 성공하였습니다."),
     // 201 Created - 클라이언트의 요청을 서버가 정상적으로 처리했고 새로운 리소스가 생겼다.,
-    SUCCESS_SELECT_PROFILE(200, 203, "프로필 조회에 성공하였습니다."),
-    SUCCESS_SELECT_BOOK(200, 210, "책 정보 조회에 성공하였습니다."),
+    SUCCESS_SELECT_PROFILE(200, 203, "프로필 조회에 성공하였습니다."), SUCCESS_SELECT_BOOK(200, 210, "책 정보 조회에 성공하였습니다."),
     SUCCESS_SELECT_USER(200, 205, "유저 조회에 성공하였습니다."),
 
     // 201 Created - 클라이언트의 요청을 서버가 정상적으로 처리했고 새로운 리소스가 생겼다.,
@@ -37,34 +31,23 @@ public enum ResponseStatus {
     ACCEPTED(202, 200, "요청에 성공하였습니다."),
 
     // 204 No Content - 클라이언트의 요청은 정상적이다. 하지만 컨텐츠를 제공하지 않는다.
-    NO_CONTENT(204, 200, "요청에 성공하였습니다."),
-    SUCCESS_UPDATE_DEAL(204, 202, "거래 수정에 성공하였습니다."),
-    SUCCESS_DELETE_DEAL(204, 203, "거래 삭제에 성공하였습니다."),
-    SUCCESS_DELETE_FOLLOW(204, 201, "팔로우 관계를 삭제하는데 성공하였습니다."),
-    SUCCESS_CHANGE_COMMENT(204, 205, "한줄평 정보를 변경하는데 성공하였습니다."),
-    SUCCESS_DELETE_COMMENT(204, 206, "한줄평 삭제에 성공하였습니다."),
+    NO_CONTENT(204, 200, "요청에 성공하였습니다."), SUCCESS_UPDATE_DEAL(204, 202, "거래 수정에 성공하였습니다."),
+    SUCCESS_DELETE_DEAL(204, 203, "거래 삭제에 성공하였습니다."), SUCCESS_DELETE_FOLLOW(204, 201, "팔로우 관계를 삭제하는데 성공하였습니다."),
+    SUCCESS_CHANGE_COMMENT(204, 205, "한줄평 정보를 변경하는데 성공하였습니다."), SUCCESS_DELETE_COMMENT(204, 206, "한줄평 삭제에 성공하였습니다."),
     SUCCESS_DELETE_LIKE_COMMENT(204, 207, "관심 한줄평 삭제에 성공하였습니다."),
 
     /*
      * 4XX Client errors
      */
     // 400 Rad Request - 클라이언트의 요청이 유효하지 않아 더 이상 작업을 진행하지 않는 경우
-    BAD_REQUEST(400, 400, "요청에 실패하였습니다."),
-    FAILED_TO_REQUEST(400, 401, "데이터를 불러오는데 실패하였습니다."),
-    FAILED_TO_CREATE_TOKEN(400, 402, "토큰 생성에 실패하였습니다."),
-    FAILED_TO_SIGN_IN(400, 403, "로그인에 실패하였습니다."),
-    EXISTS_EMAIL(400, 404, "이미 존재하는 이메일입니다."),
-    EXISTS_NICKNAME(400, 405, "이미 존재하는 닉네임입니다."),
-    NO_VALUES(400, 410, "입력되지 않은 값이 존재합니다."),
-    BAD_ACCESS_TOKEN_VALUE(400, 411, "accessToken을 입력해주세요."),
-    BAD_EMAIL_VALUE(400, 412, "올바른 이메일을 입력해주세요."),
-    BAD_PASSWORD_VALUE(400, 413, "올바른 비밀번호를 입력해주세요."),
-    BAD_NAME_VALUE(400, 414, "올바른 이름을 입력해주세요."),
-    BAD_FOLLOW_INFO_VALUE(400, 415, "올바른 팔로우 관계를 입력해주세요."),
-    BAD_SEARCH_TYPE_VALUE(400, 420, "올바른 검색 타입을 입력해주세요."),
-    BAD_ID_VALUE(400, 416, "올바른 아이디를 입력해주세요."),
-    BAD_STATUS_VALUE(400, 417, "올바른 상태를 입력해주세요."),
-    BAD_AREA_VALUE(400, 416, "올바른 지역을 입력해주세요."),
+    BAD_REQUEST(400, 400, "요청에 실패하였습니다."), FAILED_TO_REQUEST(400, 401, "데이터를 불러오는데 실패하였습니다."),
+    FAILED_TO_CREATE_TOKEN(400, 402, "토큰 생성에 실패하였습니다."), FAILED_TO_SIGN_IN(400, 403, "로그인에 실패하였습니다."),
+    EXISTS_EMAIL(400, 404, "이미 존재하는 이메일입니다."), EXISTS_NICKNAME(400, 405, "이미 존재하는 닉네임입니다."),
+    NO_VALUES(400, 410, "입력되지 않은 값이 존재합니다."), BAD_ACCESS_TOKEN_VALUE(400, 411, "accessToken을 입력해주세요."),
+    BAD_EMAIL_VALUE(400, 412, "올바른 이메일을 입력해주세요."), BAD_PASSWORD_VALUE(400, 413, "올바른 비밀번호를 입력해주세요."),
+    BAD_NAME_VALUE(400, 414, "올바른 이름을 입력해주세요."), BAD_FOLLOW_INFO_VALUE(400, 415, "올바른 팔로우 관계를 입력해주세요."),
+    BAD_SEARCH_TYPE_VALUE(400, 420, "올바른 검색 타입을 입력해주세요."), BAD_ID_VALUE(400, 416, "올바른 아이디를 입력해주세요."),
+    BAD_STATUS_VALUE(400, 417, "올바른 상태를 입력해주세요."), BAD_AREA_VALUE(400, 416, "올바른 지역을 입력해주세요."),
     EXISTS_INFO(400, 420, "이미 존재하는 정보입니다."),
 
     // 401 Unauthorized - 클라이언트가 권한이 없기 때문에 작업을 진행할 수 없는 경우
@@ -74,10 +57,8 @@ public enum ResponseStatus {
     FORBIDDEN(403, 400, "권한이 없습니다."), FORBIDDEN_USER_ID(403, 410, "해당 userId에 대한 권한이 없습니다."),
 
     // 404 Not Found - 클라이언트가 요청한 자원이 존재하지 않다.
-    NOT_FOUND(404, 400, "NOT FOUND"),
-    NOT_FOUND_USER(404, 405, "사용자 정보가 존재하지 않습니다."),
-    NOT_FOUND_FOLLOW(404, 406, "팔로우 정보가 존재하지 않습니다."),
-    NOT_FOUND_BOOK(404, 410, "책 정보가 존재하지 않습니다."),
+    NOT_FOUND(404, 400, "NOT FOUND"), NOT_FOUND_USER(404, 405, "사용자 정보가 존재하지 않습니다."),
+    NOT_FOUND_FOLLOW(404, 406, "팔로우 정보가 존재하지 않습니다."), NOT_FOUND_BOOK(404, 410, "책 정보가 존재하지 않습니다."),
     NOT_FOUND_DEAL(404, 415, "거래 정보가 존재하지 않습니다."),
 
     // 405 Method Not Allowed - 클라이언트의 요청이 허용되지 않는 메소드인 경우
